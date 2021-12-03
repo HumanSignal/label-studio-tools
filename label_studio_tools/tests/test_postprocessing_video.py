73d275f4ab0932c795702de215c1453f34ff4691
KonstantinKorotaev <konstantin@heartex.com>
2021-12-03 17:10:28 +0300

    Update extract_key_frames with latest fixes + add tests

--- label_studio_tools/tests/test_postprocessing_video.py
+++ label_studio_tools/tests/test_postprocessing_video.py
@@ -1,7 +1,6 @@
-from postprocessing.video import extract_key_frames
+from label_studio_tools.postprocessing.video import extract_key_frames
 
-
-def test_postprocessing_video_disabled_till_end():
+def test_video_disabled_till_end():
     """
     Test frames extraction with disabled in the end and frame count > disabled key frame
     """
@@ -33,7 +32,8 @@ def test_postprocessing_video_disabled_till_end():
                         "height": 34,
                         "rotation": 30
                     }
-                ]
+                ],
+                "from_name": "test"
             }
         }
     ]
@@ -44,6 +44,7 @@ def test_postprocessing_video_disabled_till_end():
     assert key_frames[0]['value']['width'] == 41
     assert key_frames[0]['value']['height'] == 22
     assert key_frames[0]['value']['rotation'] == 0
+    assert key_frames[0]['value']["from_name"] == "test"
     assert key_frames[4]['value']['x'] == 40
     assert key_frames[4]['value']['y'] == 49
     assert key_frames[4]['value']['width'] == 56
@@ -54,9 +55,12 @@ def test_postprocessing_video_disabled_till_end():
     assert key_frames[2]['value']['width'] == 48.5
     assert key_frames[2]['value']['height'] == 28
     assert key_frames[2]['value']['rotation'] == 15
+    assert key_frames[2]['value']['auto']
+    assert not key_frames[0]['value'].get('auto')
+    assert not key_frames[4]['value'].get('auto')
 
 
-def test_postprocessing_video_enabled_till_end():
+def test_video_enabled_till_end():
     """
     Test frames extraction with enabled in the end and frame count > enabled key frame
     """
@@ -109,7 +113,9 @@ def test_postprocessing_video_enabled_till_end():
     assert key_frames[2]['value']['width'] == 41
     assert key_frames[2]['value']['height'] == 22
     assert key_frames[2]['value']['rotation'] == 0
-
+    assert key_frames[2]['value']['auto']
+    assert not key_frames[0]['value'].get('auto')
+    assert not key_frames[4]['value'].get('auto')
     assert key_frames[5]['value']['x'] == 40
     assert key_frames[5]['value']['y'] == 49
     assert key_frames[5]['value']['width'] == 41
@@ -125,9 +131,12 @@ def test_postprocessing_video_enabled_till_end():
     assert key_frames[9]['value']['width'] == 41
     assert key_frames[9]['value']['height'] == 22
     assert key_frames[9]['value']['rotation'] == 0
+    assert key_frames[8]['value']['auto']
+    assert key_frames[5]['value'].get('auto')
+    assert key_frames[9]['value'].get('auto')
 
 
-def test_postprocessing_video_enabled_till_end_one_frame():
+def test_video_enabled_till_end_one_frame():
     """
     Test frames extraction with enabled in the end and frame count > enabled key frame
     """
@@ -170,7 +179,7 @@ def test_postprocessing_video_enabled_till_end_one_frame():
     assert key_frames[9]['value']['rotation'] == 0
 
 
-def test_postprocessing_video_disabled_till_end_one_frame():
+def test_video_disabled_till_end_one_frame():
     """
     Test frames extraction with disabled in the end and frame count > enabled key frame
     """
@@ -202,7 +211,7 @@ def test_postprocessing_video_disabled_till_end_one_frame():
     assert len(key_frames) == 0
 
 
-def test_postprocessing_video_disabled_till_end_2_parts():
+def test_video_disabled_till_end_keyframe_count():
     """
     Test frames extraction with disabled in the end and frame count > disabled key frame
     """
@@ -214,6 +223,76 @@ def test_postprocessing_video_disabled_till_end_2_parts():
                 "labels": [
                     "Airplane"
                 ],
+                "frameCount": 10000000,
+                "sequence": [
+                    {
+                        "frame": 1,
+                        "enabled": True,
+                        "x": 38,
+                        "y": 38,
+                        "width": 41,
+                        "height": 22,
+                        "rotation": 0
+                    },
+                    {
+                        "frame": 5,
+                        "enabled": False,
+                        "x": 40,
+                        "y": 49,
+                        "width": 41,
+                        "height": 22,
+                        "rotation": 0
+                    },
+                    {
+                        "frame": 11,
+                        "enabled": True,
+                        "x": 38,
+                        "y": 38,
+                        "width": 41,
+                        "height": 22,
+                        "rotation": 0
+                    },
+                    {
+                        "frame": 15,
+                        "enabled": False,
+                        "x": 40,
+                        "y": 49,
+                        "width": 41,
+                        "height": 22,
+                        "rotation": 0
+                    }
+                ]
+            }
+        }
+    ]
+    key_frames = extract_key_frames(example)
+    assert len(key_frames) == 10
+    assert key_frames[5]['value']['x'] == 38
+    assert key_frames[5]['value']['y'] == 38
+    assert key_frames[5]['value']['width'] == 41
+    assert key_frames[5]['value']['height'] == 22
+    assert key_frames[5]['value']['rotation'] == 0
+    assert key_frames[9]['value']['x'] == 40
+    assert key_frames[9]['value']['y'] == 49
+    assert key_frames[9]['value']['width'] == 41
+    assert key_frames[9]['value']['height'] == 22
+    assert key_frames[9]['value']['rotation'] == 0
+    assert key_frames[7]['value']['x'] == 39
+    assert key_frames[7]['value']['y'] == 43.5
+    assert key_frames[7]['value']['width'] == 41
+    assert key_frames[7]['value']['height'] == 22
+    assert key_frames[7]['value']['rotation'] == 0
+
+
+def test_no_label_result():
+    """
+    Test frames extraction with no label and disabled in the end and frame count > disabled key frame
+    """
+    example = [
+        {
+            "id": "tJhYZLMC9G",
+            "type": "videorectangle",
+            "value": {
                 "frameCount": 10000000,
                 "sequence": [
                     {
